64bd441988d0372683ca56116f9e1c039138fa82
Andrey Gordeev <jgordeev@gmail.com>
2015-12-30 23:13:44 +0700

    refactor(*): remove unuse code

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,12 @@
-$(function(){
-	
-});
+(function() {
+  'use strict'
+  var buttonStart = document.querySelector(".js-startPlay"),
+      checkboxPlay = document.getElementById("checkPlay");
+  
+  function disableButton() {
+    buttonStart.classList.toggle("is-disabled");
+  }
+
+  checkboxPlay.addEventListener("click", disableButton);
+
+})();
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 var gulp = require('gulp'),
 	jade = require('gulp-jade'),
 	postcss = require('gulp-postcss'),
-	precss = require('precss'),
+  precss = require('precss'),
+  cssnext = require('cssnext'),
+	autoprefixer = require('autoprefixer'),
 	colorFunction = require("postcss-color-function"),	
 	browserSync = require('browser-sync').create(),
- 	mainBowerFiles = require('main-bower-files'),
 	rename = require("gulp-rename"),
 	plumber = require('gulp-plumber'),	
 	notify = require('gulp-notify');
@@ -31,6 +32,8 @@ gulp.task('template', function(){
 gulp.task('postcss',  function() {
 	var processors = [
 			precss,
+      autoprefixer({ browsers: ['last 3 versions'] }),
+      cssnext,
 			colorFunction
 		];
     return gulp.src(['app/css/main.css'])
@@ -45,33 +48,8 @@ gulp.task('postcss',  function() {
 gulp.task('js', function(){
 	gulp.src("app/js/main.js")
 	.pipe(gulp.dest("dist/js/"))
+  .pipe(notify('JS update'))
 });
-
-// main bower files
-gulp.task('fontsMainBower', function() {
-	return gulp.src(mainBowerFiles(['**/*.eot', '**/*.svg', '**/*.ttf', '**/*.woff', '**/*.woff2', '**/*.otf']))
-	    .pipe(gulp.dest('dist/css/fonts'))
-});
-gulp.task('cssMainBower', function() {
-	return gulp.src(mainBowerFiles('**/*.css'))
-	    .pipe(gulp.dest('app/css/vendor'))
-});
-gulp.task('jsMainBower', function() {
-	return gulp.src(mainBowerFiles('**/*.js'))
-	    .pipe(gulp.dest('dist/js/vendor'))
-});
-gulp.task('allMainBower',['fontsMainBower', 'cssMainBower', 'jsMainBower']);
-
-// image optimization
-gulp.task('image', function() {
-     gulp.src('dist/img/*')
-       .pipe(gulp.dest('raw/images'))
-       .pipe(imagemin({
-       		progressive: true,
-       		use: [pngquant()]
-       }))
-       .pipe(gulp.dest('dist/img'));
- });
 
 // convert from HTML to Jade
  gulp.task('convert', function(){
@@ -81,42 +59,25 @@ gulp.task('image', function() {
  		.pipe(gulp.dest('raw/builder/jade'));
  });
 
-
+// watcher
 gulp.task('watch', function() {
-   gulp.watch(['app/css/*.css', 'app/css/**/*.css', 'app/templates/*.jade', 'app/templates/**/*.jade'], ['postcss', 'template']).on("change", browserSync.reload);
-});
-
-gulp.task('convert', function(){
-	gulp.src('raw/builder/html/*.html')
-		.pipe(html2jade())
-	.pipe(notify('Шаблоны .jade обновлены'))
-		.pipe(gulp.dest('raw/builder/jade'));
-});
-
-gulp.task('fonts', function(){
-	gulp.src("app/icons/*.svg")
-	.pipe(iconfont({
-	  fontName: 'iconproject',
-	  appendCodepoints: true  
-	}))
-	.on('codepoints', function(codepoints, options){
-		console.log(codepoints, options);
-	})
-	.pipe(gulp.dest("dist/fonts/iconproject"))
-});
-
-// include fonts
-gulp.task('tofonts1', function(){
-	gulp.src("bower_components/fontawesome/fonts/*")
-	.pipe(gulp.dest("dist/fonts/fontawesome"))
-});
-gulp.task('tofonts2', function(){
-	gulp.src("bower_components/roboto-fontface/fonts/*")
-	.pipe(gulp.dest("dist/fonts/roboto-fontface"))
+  gulp.watch([
+      'app/css/*.css', 
+      'app/css/**/*.css', 
+      'app/templates/*.jade', 
+      'app/templates/**/*.jade',
+      'app/js/*.js'
+    ], 
+    [
+      'postcss', 
+      'template',
+      'js'
+    ])
+    .on("change", browserSync.reload);
 });
 
 // default task
 gulp.task('default', ['browser-sync', 'postcss', 'template', 'js', 'watch']);
 
 // other tasks
-gulp.task('update', [ 'mainBower', 'postcss', 'template', 'fonts']);
+gulp.task('update', [ 'mainBower', 'postcss', 'template']);
